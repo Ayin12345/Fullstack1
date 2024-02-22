133ad95c9057d3d670fdde2547ce64a6404c11b9
Ayin12345 <119428134+Ayin12345@users.noreply.github.com>
2024-02-21 18:03:45 -0800

    Update 1.13.jsx

--- 1.13.jsx
+++ 1.13.jsx
@@ -1,45 +1,44 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
-const App = () => {
+const App = (props) => {
   const anecdotes = [
-    'If it hurts, do it more often.',
+    'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-  const points = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
-  const [votes, setVotes] = useState({...points})
-   
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
-  const buttonClick = () => {
-    setSelected(Math.floor(Math.random() * 8))
-  }
-  const vote = () => {
-    setVotes({...votes, [selected]: votes[selected] + 1})
-  }
-  
-  return (
-    <div>
-      {anecdotes[selected]}
-      <br></br>
-      Has {votes[selected]} votes
-      <br></br>
-      <Button handleClick={buttonClick} text="next anecdote"/>
-      <Button handleClick={vote} text="vote"/>
-    </div>
-  )
+    const [selected, setSelected] = useState(0);
+
+    const votes = new Array(8).fill(0);
+    const [points, setPoints] = useState(votes);
+
+    const [maxIndex, setMaxIndex] = useState(0);
+
+    const handleClick = (action) => () => {
+        if (action === "vote") {
+            const copy = [...points];
+            copy[selected] += 1;
+            setPoints(copy);
+        } else if (action === "next") {
+            setSelected(Math.floor(Math.random() * anecdotes.length));
+        }
+    }
+
+
+    return (
+        <div>
+            <h1>Anecdote of the day</h1>
+            <p>{anecdotes[selected]}</p>
+            <p>has {points[selected]}</p>
+            <button onClick={handleClick("vote")}>Vote</button>
+            <button onClick={handleClick("next")}>Next Anecdote</button>
+        </div>
+    )
 }
+
 
 export default App
